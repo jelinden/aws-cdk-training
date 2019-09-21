ddc7dff069f4a199b9bc4ea544d15696819faf1e
Jouni Linden <jouni.linden@gmail.com>
2019-09-21 09:32:17 +0300

    Initial commit

---
+++ bin/aws-cdk-training.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import cdk = require('@aws-cdk/core');
+import { AwsCdkTrainingStack } from '../lib/aws-cdk-training-stack';
+
+const app = new cdk.App();
+new AwsCdkTrainingStack(app, 'AwsCdkTrainingStack');
